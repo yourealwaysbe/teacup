68c04a0be19ce2f6760a5890a65c8c4640102d38
Matthew Hague <matthewhague@zoho.com>
2012-12-31 17:55:21 +0100

    loads album art from music file directory if not found in mp3, also scales bitmaps for space saving

--- src/net/chilon/matt/teacup/TeaCupReceiver.java
+++ src/net/chilon/matt/teacup/TeaCupReceiver.java
@@ -1,5 +1,8 @@
 package net.chilon.matt.teacup;
 
+import java.io.File;
+import java.io.FileFilter;
+
 import net.chilon.matt.teacup.R;
 import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
@@ -24,6 +27,9 @@ public class TeaCupReceiver extends BroadcastReceiver {
 	private static final String INTENT_ARTIST = "artist";
 	private static final String INTENT_TRACK = "track";
 	private static final String INTENT_PLAYING = "playing";
+	
+	private static final int ART_WIDTH = 72;
+	private static final int ART_HEIGHT = 72;
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -60,12 +66,79 @@ public class TeaCupReceiver extends BroadcastReceiver {
 	
 	private Bitmap getArtBmp(Context context, Intent intent) {
 		Bitmap artBmp = null;
+		
+		String playingFilename = getPlayingFilename(context, intent);
 	    	
+	    if (playingFilename != null) {
+	    	artBmp = getFileEmbeddedArt(playingFilename);
+	    	if (artBmp == null) {
+	    		artBmp = getImageFromDirectory(playingFilename);
+	    	}
+	    }
+	    	
+	    if (artBmp == null) {
+	    	artBmp = getDefaultArt(context);
+	    }
+
+	    return artBmp;	
+	}
+	
+	private Bitmap getImageFromDirectory(String filename) {
+		Bitmap artBmp = null;
+		
+		File file = new File(filename);
+		String directory = file.getParent();
+		
+		if (directory != null) {
+			FileFilter imageFilter = new FileFilter() {
+				public boolean accept(File file) {
+					String filename = file.getName();
+					return filename.endsWith(".jpg") ||
+							filename.endsWith(".jpeg") ||
+							filename.endsWith(".bmp") ||
+							filename.endsWith(".png");
+				}
+			};
+	    
+			File[] files = new File(directory).listFiles(imageFilter);
+			for (int i = 0; i < files.length && artBmp == null; ++i) {
+				artBmp = decodeSampledBitmap(files[i],  
+						                     ART_WIDTH, 
+						                     ART_HEIGHT);
+			}
+		}
+		return artBmp;
+	}
+	
+	
+	private Bitmap getDefaultArt(Context context) {
+		return BitmapFactory.decodeResource(context.getResources(), 
+                                            R.drawable.ic_blankalbum);
+	}
+
+	private Bitmap getFileEmbeddedArt(String filename) {
+		Bitmap artBmp = null;
+		
+		MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+		retriever.setDataSource(filename);
+		byte[] artArray = retriever.getEmbeddedPicture();
+		if (artArray != null) {
+			artBmp = decodeSampledBitmapFromBytes(artArray,
+					                              ART_WIDTH,
+					                              ART_HEIGHT);
+		}
+		
+		return artBmp;
+	}
+	
+	private String getPlayingFilename(Context context, Intent intent) {
 	    long id = intent.getLongExtra(INTENT_TRACK_ID, -1);
-	    	
+    	
+	    String filename = null;
+	    
 	    if (id  >= 0) {
 	    	String selectionArgs[] = {
-	    		new Long(id).toString()
+	    		Long.toString(id)
 	    	};
 	    	String projection[] = {
 	    		MediaStore.Audio.Media.DATA
@@ -80,24 +153,11 @@ public class TeaCupReceiver extends BroadcastReceiver {
 	    	Cursor result = q.loadInBackground();
 	    	if (result.getCount() > 0) {
 	    		result.moveToFirst();
-	    		String fileName = result.getString(0);
-	    		MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-	    		retriever.setDataSource(fileName);
-	    		byte[] artArray = retriever.getEmbeddedPicture();
-	    		if (artArray != null) {
-	    			artBmp = BitmapFactory.decodeByteArray(artArray, 
-	    					                               0, 
-	    					                               artArray.length);
-	    		}
+	    		filename = result.getString(0);	
 	    	}
 	    }
-	    	
-	    if (artBmp == null) {
-	    	artBmp = BitmapFactory.decodeResource(context.getResources(), 
-	    				                          R.drawable.ic_blankalbum);
-	    }
-
-	    return artBmp;	
+	    
+	    return filename;
 	}
 	
 	private void updateWidget(Context context,
@@ -118,5 +178,53 @@ public class TeaCupReceiver extends BroadcastReceiver {
 	    ComponentName thisWidget = new ComponentName(context, TeaCup.class);
 	    appWidgetManager.updateAppWidget(thisWidget, views);
 	}
+	
+	// code below from android tutorial:
+	// https://developer.android.com/training/displaying-bitmaps/load-bitmap.html
 
+	private static Bitmap decodeSampledBitmap(File file,
+	                                          int reqWidth, 
+	                                          int reqHeight) {
+		String path = file.getPath();
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(path, options);
+
+		options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+		options.inJustDecodeBounds = false;
+		options.inPurgeable = true;
+		return BitmapFactory.decodeFile(path, options);
+	}
+	
+	private static Bitmap decodeSampledBitmapFromBytes(byte[] data,
+                                                       int reqWidth, 
+                                                       int reqHeight) {
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeByteArray(data, 0, data.length);
+		options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+		options.inJustDecodeBounds = false;
+		options.inPurgeable = true;
+		return BitmapFactory.decodeByteArray(data, 0, data.length);
+	}
+
+	public static int calculateInSampleSize(BitmapFactory.Options options, 
+                                            int reqWidth, 
+                                            int reqHeight) {
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+			if (width > height) {
+				inSampleSize = Math.round((float)height / (float)reqHeight);
+			} else {
+				inSampleSize = Math.round((float)width / (float)reqWidth);
+			}
+		}
+		
+		return inSampleSize;
+	}
+
+	
 }
